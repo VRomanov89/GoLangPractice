b87627284433852cbbb467cca4169ba56e05fdd4
Vladimir Romanov <vladromweb@gmail.com>
2022-06-09 23:40:05 -0400

    Slices
    Slices

--- 01. DataCollections/lists.go
+++ 01. DataCollections/lists.go
@@ -7,9 +7,5 @@ func main() {
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 	fmt.Println(prices)
 	fmt.Println(productNames)
-	fmt.Println(prices[0])
-
-	featuredPrices := prices[1:3] // Slice of an array
-	fmt.Println(featuredPrices)
 
 }
