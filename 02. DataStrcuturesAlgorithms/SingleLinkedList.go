76d2d49c1a5677f524a6948470aaf8d3d92dfed5
Vladimir Romanov <vladromweb@gmail.com>
2022-06-14 16:56:04 -0400

    HeapStart
    HeapStart

--- 02. DataStrcuturesAlgorithms/SingleLinkedList.go
+++ 02. DataStrcuturesAlgorithms/SingleLinkedList.go
@@ -30,19 +30,47 @@ func (l linkedList) printListData() {
 	fmt.Printf("\n")
 }
 
+func (l *linkedList) deleteWithValue(value int) {
+	//Handle empty list case
+	if l.length == 0 {
+		return
+	}
+
+	//Handle header == search value
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
+	//Handle no value found
+
+	previousToDelete := l.head
+	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
+		previousToDelete = previousToDelete.next
+	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
+}
+
 func main() {
 	myList := linkedList{}
 	node1 := &node{data: 48}
 	node2 := &node{data: 18}
 	node3 := &node{data: 416}
-	node4 := &node{data: 416}
-	node5 := &node{data: 416}
-	node6 := &node{data: 416}
+	node4 := &node{data: 32}
+	node5 := &node{data: 24}
+	node6 := &node{data: 24}
 	myList.prepend(node1)
 	myList.prepend(node2)
 	myList.prepend(node3)
 	myList.prepend(node4)
 	myList.prepend(node5)
 	myList.prepend(node6)
+	myList.printListData()
+	myList.deleteWithValue(30)
 	myList.printListData()
 }
